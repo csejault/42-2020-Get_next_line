7ba9dd0ce88d30eaf845b24658003e89a46187dd
VM42 - csejault <clement.sejault@outlook.com>
2020-12-09 19:04:36 +0100

    fix: get_next_line_bonus.c - add bonus to the new get_next_line.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: csejault <csejault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 16:20:45 by csejault          #+#    #+#             */
-/*   Updated: 2020/12/09 11:39:58 by csejault         ###   ########.fr       */
+/*   Updated: 2020/12/09 19:00:12 by csejault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 ** On guacamole, it is 256.
 */
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
 int		fill_line(char **line, t_gnl *gnl, size_t i)
 {
@@ -62,12 +62,11 @@ int		read_file(int fd, t_gnl *gnl)
 	}
 	buf[gnl->retreadf] = '\0';
 	if (gnl->cache)
-	{
 		gnl->cache = ft_strjoin(gnl->cache, buf);
-		free(tofree);
-	}
 	else
-		gnl->cache = ft_strdup(buf);
+		gnl->cache = ft_substr(buf, 0, ft_strlen(buf));
+	if (tofree)
+		free(tofree);
 	free(buf);
 	if (!gnl->cache)
 		return (gnl->retreadf = -1);
@@ -77,30 +76,30 @@ int		read_file(int fd, t_gnl *gnl)
 int		read_cache(t_gnl *gnl, int fd, char **line)
 {
 	size_t	i;
-	int		ret;
 
-	ret = 0;
 	i = 0;
 	if (!gnl->retreadf && !gnl->cache[0])
 	{
-		if (!(*line = ft_strdup("")))
+		*line = ft_substr("", 0, 0);
+		if (gnl->cache)
+		{
+			free(gnl->cache);
+			gnl->cache = NULL;
+		}
+		if (!(*line))
 			return (-1);
 		return (0);
 	}
 	while (gnl->cache[i] && gnl->cache[i] != '\n')
 		i++;
 	if (gnl->cache[i] == '\n')
-		return (ret = fill_line(line, gnl, i));
-	else if (gnl->cache[i] == '\0')
-	{
+		return (fill_line(line, gnl, i));
+	if (gnl->cache[i] == '\0')
 		if (gnl->retreadf < BUFFER_SIZE)
 			return (fill_line(line, gnl, i));
-		else if (0 > (read_file(fd, gnl)))
-			return (-1);
-		else
-			return (read_cache(gnl, fd, line));
-	}
-	return (-1);
+	if (0 > (read_file(fd, gnl)))
+		return (-1);
+	return (read_cache(gnl, fd, line));
 }
 
 int		get_next_line(int fd, char **line)
@@ -113,18 +112,13 @@ int		get_next_line(int fd, char **line)
 		return (-1);
 	*line = NULL;
 	if (!gnl[fd].cache)
-	{
 		if (0 > read_file(fd, &(gnl[fd])))
 			return (-1);
-		gnl[fd].init = 1;
-	}
-	if (0 > (retcache = read_cache(&(gnl[fd]), fd, line)))
+	retcache = read_cache(&(gnl[fd]), fd, line);
+	if (gnl[fd].cache && retcache != 1)
 	{
-		if (gnl[fd].cache)
-			free(gnl[fd].cache);
-		return (-1);
+		free(gnl[fd].cache);
+		gnl[fd].cache = NULL;
 	}
-	else
-		return (retcache);
-	return (0);
+	return (retcache);
 }
