6ed6fbfdeed9126c61e4c140661aa9bc655afcb4
VM42 - csejault <clement.sejault@outlook.com>
2020-12-09 12:26:18 +0100

    feat: get_next_line_bonus.h - copy of get_next_line.h

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csejault <csejault@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/09 10:48:02 by csejault          #+#    #+#             */
+/*   Updated: 2020/12/09 11:38:00 by csejault         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+typedef struct	s_gnl
+{
+	int			init;
+	int			retreadf;
+	char		*cache;
+}				t_gnl;
+
+int				get_next_line(int fd, char **line);
+size_t			ft_strlen(const char *str);
+char			*ft_strdup(const char *s);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
+#endif
